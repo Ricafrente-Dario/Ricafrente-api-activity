3f48fcf2d485bc0d9fd0913d124cd74cd7e542b0
unknown <ricafrentejr.dario@marsu.edu.ph>
2026-03-11 23:30:33 +0800

    nag delete nga

--- test-mongo.js
+++
@@ -1,7 +0,0 @@
-// test-mongo.js
-require('dotenv').config();
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected!"))
-  .catch(err => console.error("❌ MongoDB connection failed:", err));
